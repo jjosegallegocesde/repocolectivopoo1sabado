fa6be378555e61e41de20bf60a99a9f69672da19
Luis Miguel <luisariasarias0358@gmail.com>
2023-04-15 12:42:41 -0500

    Nueva rama primer versionamiento

---
+++ Ander.py
@@ -0,0 +1,11 @@
+class Poo:
+    #declaro la funcion especial construtor que se encraga de inicializar mis atributos.
+    def __init__(self):   #ESTE ES EL CONSTRUCTOR
+        #DEFINO LOS ATRIBUTOS DE MI CLASE ATRIBUTOS=VARIABLES...
+        self.nombre='Anderson yepes'
+        self.edad=24
+        self.estudiantePlatzi='Node.js'
+        self.jugarFutbol=True
+    
+    def analitico(self):
+        print('Programador Backenn')
