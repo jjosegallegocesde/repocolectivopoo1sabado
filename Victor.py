ef3d99957091bd0d61d571df506f68bdb824a8b2
Victor Suarez <vasuarezag@cesde.net>
2023-04-15 12:50:03 -0500

    NTS-9victor: se crea clase propia

---
+++ Victor.py
@@ -0,0 +1,10 @@
+class Victor:
+    def __init__(self):
+        self.nombre="Victor Suarez"
+        self.edad=36
+        self.saldoEnCuenta=200000
+        self.yaAlmorzo=False
+        
+    def trabajar():
+        print("Hola esto trabajando")
+        
