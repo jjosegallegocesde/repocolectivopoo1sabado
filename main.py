0d2262caebb0cb501429379959d450f7259c3ab9
wilyCesde <wruizza@cesde.net>
2023-04-15 12:49:19 -0500

    nt9 se crea clase propia

---
+++ main.py
@@ -0,0 +1,17 @@
+from Willian import Willian
+
+# PARA USUAR UNA CLASE DEBO CREAR UNA INSTANCIA
+
+# UN OBJETO ES UNA VARIABLE PERO (ESPECIAL)
+
+objeto = Willian()
+
+# COMO USO EL OBJETO
+
+# LLAMO LOS ATRIBUTOS
+
+objeto.nombre = "willian"
+
+objeto.edad=35
+
+objeto.saludar()
