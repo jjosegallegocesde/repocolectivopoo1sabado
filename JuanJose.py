daeb5af98e60c35e04c66cb0f27e427a9bd91736
dicojo <diniercj@gmail.com>
2023-04-15 12:41:57 -0500

    NT9 se crea clase propia

---
+++ JuanJose.py
@@ -0,0 +1,11 @@
+class JuanJose:
+
+    def __init__(self):
+        self.nombre="Juan Jose"
+        self.edad=33
+        self.saldoEnCuenta=-100
+        self.yaAlmorzo=False
+
+    def quedarseSinVoz():
+        print("hola estoy sin voz")
+
