343013828fc9af91ed1955128ff293bc16fd1771
Your Name <you@example.com>
2023-04-15 12:46:17 -0500

    NT9 se crea clase propia

---
+++ Miguel.py
@@ -0,0 +1,9 @@
+class Miguel:
+    def __init__(self):
+        self.nombre="Miguel Angel Marmol"
+        self.edad="28"
+        self.altura="180"
+        self.peso="78"
+    
+    def quehaces():
+        print("Estoy estudiando")
