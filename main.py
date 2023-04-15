e6cbe8d86e07ab85da9d3be559cfba2b3fe13886
Andrés Rengifo <andresm.rengifo@gmail.com>
2023-04-15 12:48:24 -0500

    Se crea clase propia

---
+++ main.py
@@ -0,0 +1,8 @@
+from AndresFelipe import AndresFelipe;
+
+andresFelipe = AndresFelipe();
+
+andresFelipe.color = "Negro";
+andresFelipe.edad = "20";
+andresFelipe.estatura = "1.82";
+andresFelipe.nombre = "Andrés"
