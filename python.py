5131a31994c41be6837866d8237bab24fad3acab
marianaalvarezr17 <mariana.mar170906@gmail.com>
2023-04-15 12:44:27 -0500

    Se crea clase

---
+++ python.py
@@ -0,0 +1,8 @@
+class LuisMiguel:
+    def __init__(self):
+        self.nombre = 'Luis Miguel'
+        self.edad = 20
+        self.SaldoDecuenta = 12456678
+        self.Liquidacion = 145799562
+
+    
